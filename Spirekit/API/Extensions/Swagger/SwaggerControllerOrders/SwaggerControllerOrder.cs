3909fcbf8dafc1712766bd1d2500610761e33569
Tiago Barracha <taigo.barracha@gmail.com>
2025-06-14 13:15:26 +0100

    feat: added swagger controller order attribute

---
+++ Spirekit/API/Extensions/Swagger/SwaggerControllerOrders/SwaggerControllerOrder.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+
+namespace Spirekit.API.Extensions.Swagger.SwaggerControllerOrders;
+
+/// <summary>
+/// Builds a lookup (controller-name ➜ sort-order) by scanning assemblies
+/// for controllers that carry <see cref="SwaggerControllerOrderAttribute"/>.
+/// </summary>
+public sealed class SwaggerControllerOrder
+{
+    private readonly Dictionary<string, int> _orderLookup;
+
+    public SwaggerControllerOrder(IEnumerable<Type> controllerTypes)
+    {
+        _orderLookup = controllerTypes
+            .Select(t => new
+            {
+                Name = ResolveControllerName(t.Name),
+                Order = t.GetCustomAttribute<SwaggerControllerOrderAttribute>()!.Order
+            })
+            .ToDictionary(x => x.Name, x => x.Order, StringComparer.OrdinalIgnoreCase);
+    }
+
+    public int GetOrder(string controllerName) =>
+        _orderLookup.TryGetValue(controllerName, out var order) ? order : int.MaxValue;
+
+    public string SortKey(string controllerName) =>
+        $"{GetOrder(controllerName):D10}_{controllerName}";
+
+    /// <summary>Controller types == *public* classes that end with "Controller".</summary>
+    public static IEnumerable<Type> DiscoverControllers(Assembly? assembly)
+    {
+        return (assembly ?? Assembly.GetExecutingAssembly())
+               .GetExportedTypes()
+               .Where(t => t.IsClass && t.Name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase));
+    }
+
+    private static string ResolveControllerName(string name) =>
+        name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase)
+            ? name[..^"Controller".Length]
+            : name;
+}
