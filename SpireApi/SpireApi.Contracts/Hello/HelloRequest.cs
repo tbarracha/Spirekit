10b8381a81587b66daac9de086c143ffbc0c2db9
Tiago Barracha <taigo.barracha@gmail.com>
2025-07-01 20:35:21 +0100

    removed SpireApi.Template (not yet ready)

--- SpireApi/SpireApi.Contracts/Hello/HelloRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SpireApi.Contracts.Hello;
-
-public class HelloRequest
-{
-    public string Name { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-}
